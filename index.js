f0731f9509efab0ee63edff1204b3a70cdd29276
John Bohannon <imjohnbo@github.com>
2019-11-06 11:23:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var cacheManager = require('cache-manager');
-var memoryCache = cacheManager.caching({store: 'memory', max: 100, ttl: 10/*seconds*/});
-var ttl = 5;
+const { Application } = require('probot')
+
+console.log(Application);
+
+// var cacheManager = require('cache-manager');
+// var memoryCache = cacheManager.caching({store: 'memory', max: 100, ttl: 10/*seconds*/});
+// var ttl = 5;
